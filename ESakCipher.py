7acff342dc5f8540dbc330a579c1cfcb926ef08c
matlev1111 <60525820+matlev1111@users.noreply.github.com>
2023-02-04 20:46:00 +0200

    Esak Cipher added and tested

--- ESakCipher.py
+++ ESakCipher.py
@@ -1,21 +1,11 @@
-"""
-function numbers = genG(p)
-q = (p-1)/2;
-%q = 2^19-1;
-index = 3;
-while(powermod(index,q,p)~= 1 && mod(powermod(index,1,p)*powermod(index,2,p),p)~=powermod(index,1,p))
-index = index + 1;
-end
-numbers = [];
-for i=1:q
-    numbers = [numbers powermod(index,i,p)];
-end
-end
-"""
 import numpy as np
 from sympy.ntheory import discrete_log
+from main_func import *
 
 def GenG(p):
+    """
+    Generates index and number list of the mapping function
+    """
     q = (p-1)//2
     index = 3
     while (np.mod(np.power(index,q),p) != 1 and np.mod(np.mod(np.power(index,1),p) * np.mod(np.power(index,2),p),p) != np.mod(np.power(index,1),p)):
@@ -27,6 +17,9 @@ def GenG(p):
 
 
 def Gmap(index, mat):
+    """
+    Maps matrix elements using function f
+    """
     m = len(mat)
     M = np.zeros(m)
     fm = np.vectorize(lambda x: index[x])
@@ -34,9 +27,28 @@ def Gmap(index, mat):
     return M
 
 def imap(ind, num,p):
+    """
+    Obtains pre-image of each matrix element
+    """
     m = len(num)
     M = np.zeros(m)
     fm = np.vectorize(lambda x: discrete_log(p, x, ind))
     M=fm(num)
     return M
+
+def ESakCipher(G,X,M,Y,p):
+    """
+    G = GenG(p)
+    X \in 
+    M \in 
+    Y \in Z_q \ {0}
+    """
+    q = (p-1)//2
+    Fmap = Gmap(G,X)
+    C1 = np.mod(X+M,q)
+    F = Gmap(G,C1)
+    YF = matrix_exp_left(F,Y,p)
+    YFY = matrix_exp_right(YF,Y,p)
+    C2 = hadamard_prod(Fmap, YFY,p)
+    return C2
 
--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 from main_func import *
+from ESakCipher import *
 
 W = np.matrix([[1,2],[3,4]])
 p = np.int32(7)
@@ -6,3 +7,15 @@ X = np.matrix([[1,2],[3,4]])
 
 print(matrix_exp_left(W,X,p))
 print(matrix_exp_right(W,X,p))
+
+
+p = 11
+q = (p-1)//2
+m = 4
+ind, G = GenG(p)
+X = np.random.randint(0,q,(m,m))
+Y = np.random.randint(1,q,(m,m))
+M = [[2,2,3,3],[2,2,4,4],[2,2,3,3],[2,2,4,4]]
+
+ats = ESakCipher(G,X,M,Y,p)
+print(ats)
